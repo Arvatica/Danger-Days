d42a97d33b81af8f6b263bd3322e50dc6cfbffd8
Arvatica <arvatica@gmail.com>
2023-05-23 11:54:28 -0500

    smene

---
+++ Danger Days/Assets/Scenes/LevelChange.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+
+    public void Nivel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Nivel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Nivel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Nivel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Nivel5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Nivel6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Nivel7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void Nivel8()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+
+}
