732274d11bb6293ec8c199658431e5ffb04407a8
Arvatica <arvatica@gmail.com>
2023-05-22 23:44:36 -0500

    asdassd

---
+++ Danger Days/Assets/Scenes/Scripts/Credits.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public void ArvaticaYoutube()
+    {
+        Application.OpenURL("https://www.youtube.com/@Arvatica");
+    }
+    public void ArvaticaTwitter()
+    {
+        Application.OpenURL("https://twitter.com/Arvatica1");
+    }
+    public void ArvaticaInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/arvatica/");
+    }
+    public void IkarusInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/meinuel.art/");
+    }
+    public void LunariaTwitter()
+    {
+        Application.OpenURL("https://twitter.com/Sparkie_illust/media");
+    }
+    public void LunariaInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/sparkie_sheesh/");
+    }
+}
+
+
